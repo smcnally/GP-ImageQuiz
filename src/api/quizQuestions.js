e5a0c744b01c872ba4daba9deb15f93fc0ea9c9c
tcasey <trevort.casey@gmail.com>
2016-09-25 22:35:37 -0600

    :sparkles: functional :sparkles:

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,160 @@
+var quizQuestions = [
+  {
+    // Group 2
+      question: "Do you have a hard time at work?",
+      answers: [
+          {
+              type: "Michael",
+              content: "That's what she said"
+          },
+          {
+              type: "Meredith",
+              content: "Only when I don't have a drink"
+          },
+          {
+              type: "Dwight",
+              content: "Never"
+          },
+          {
+              type: "Jim",
+              content: "I'd say the only hard thing at work is getting motivated to actually work"
+          },
+          {
+              type: "Ryan",
+              content: "Wait, work's supposed to be hard?"
+          }
+      ]
+  },
+  {
+    // Group 1
+      question: "What got you in trouble in high school?",
+      answers: [
+          {
+              type: "Toby",
+              content: "you didn't get in trouble"
+          },
+          {
+              type: "Kevin",
+              content: "eating in class"
+          },
+          {
+              type: "Stanley",
+              content: "sleeping"
+          },
+          {
+              type: "Kelly",
+              content: "talking"
+          },
+          {
+              type: "Oscar",
+              content: "arguing with teachers"
+          }
+      ]
+  },
+  {
+    // Group 1
+      question: "How will you probably die?",
+      answers: [
+          {
+              type: "Stanley",
+              content: "in your sleep"
+          },
+          {
+              type: "Kevin",
+              content: "sugar overdose"
+          },
+          {
+              type: "Toby",
+              content: "alone"
+          },
+          {
+              type: "Kelly",
+              content: "murdered"
+          },
+          {
+            type: "Oscar",
+            content: "exhaustion"
+          }
+      ]
+  },
+  {
+    // Group 2
+      question: "What is your dream occupation?",
+      answers: [
+        {
+          type: "Dwight",
+          content: "farmer"
+        },
+        {
+            type: "Jim",
+            content: "sports agent"
+        },
+        {
+          type: "Michael",
+          content: "comedian"
+        },
+        {
+            type: "Meredith",
+            content: "bar tender"
+        },
+        {
+          type: "Ryan",
+          content: "senior management"
+        }
+      ]
+  },
+  {
+    // Group 2
+      question: "What do you bring for the holiday gift-exchange?",
+      answers: [
+          {
+              type: "Michael",
+              content: "something expensive"
+          },
+          {
+              type: "Dwight",
+              content: "a last minute gift"
+          },
+          {
+              type: "Ryan",
+              content: "a re-gift from last year"
+          },
+          {
+              type: "Jim",
+              content: "something thoughtful and meaningful"
+          },
+          {
+              type: "Meredith",
+              content: "alcohol"
+          }
+      ]
+  },
+  {
+    // Group 2
+      question: "You have a crush on someone what do you do?",
+      answers: [
+          {
+              type: "Michael",
+              content: "force yourself on them, they’ll HAVE to love you"
+          },
+          {
+              type: "Dwight",
+              content: "boldly ask them out"
+          },
+          {
+              type: "Ryan",
+              content: "get your friends to set you up"
+          },
+          {
+              type: "Jim",
+              content: "secretly hope they notice you"
+          },
+          {
+              type: "Meredith",
+              content: "hit on them until it either works or fails horribly"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
