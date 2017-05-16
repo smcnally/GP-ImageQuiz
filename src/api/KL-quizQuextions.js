eed44d325cd0dd38cc550591f5071bc99175ae76
Steve McNally <smcnally@gmail.com>
2017-05-16 12:59:23 -0400

    execution-specific updates

---
+++ src/api/KL-quizQuextions.js
@@ -0,0 +1,46 @@
+var quizQuestions = [
+  {
+    // Group 1
+      question: "In Harvard's study, after three months of medical cannabis treatment, patients' speed and accuracy on cognitive tasks got",
+      answers: [
+          {
+              type: "Improved",
+              content: "Better",
+              imageSrc: "https://meanbusiness.com/shepherd/src/svg/cognitive-task-improvement.gif"
+          },
+          {
+              type: "Deteriorated",
+              content: "Worse",
+              imageSrc: "https://meanbusiness.com/files/2017/05/cognitive-worse.jpeg"
+          },
+          {
+              type: "Same",
+              content: "Stayed the Same",
+              imageSrc: "https://meanbusiness.com/files/2017/05/cognitive-same.jpg"
+          },
+      ]
+  },
+  {
+    // Group 1
+      question: "Opioid use as reported by patients",
+      answers: [
+          {
+              type: "Same",
+              content: "Remained the Same",
+              imageSrc: "https://meanbusiness.com/shepherd/src/svg/gifs/Bey-RidingBike.gif"
+          },
+          {
+              type: "Deteriorated",
+              content: "Increased by 15%",
+              imageSrc: "https://meanbusiness.com/shepherd/src/svg/gifs/Dinner-With-Friends.gif"
+          },
+          {
+              type: "Improved",
+              content: "Decreased by 42%",
+              imageSrc: "https://meanbusiness.com/shepherd/src/svg/WatchingTelevision-TV.jpg"
+          },
+      ]
+  }
+ ];
+
+export default quizQuestions;
---
+++ src/api/SusanK-quizQuestions.js
@@ -0,0 +1,74 @@
+var quizQuestions = [
+  {
+    // Group 1
+      question: "Do you take meds for any of these conditions? (Pick one)",
+      answers: [
+          {
+              type: "Bluebird",
+              content: "Insomnia",
+              imageSrc: "https://meanbusiness.com/files/2017/05/Insomnia-Woman.jpg"
+          },
+          {
+              type: "Palmetto",
+              content: "Anxiousness",
+              imageSrc: "https://meanbusiness.com/files/2017/05/woman-anxiety-300.gif"
+          },
+		  {
+              type: "Sativa",
+              content: "[None of These, Thank Heavens!]",
+              imageSrc: "https://typography.guru/xmodules/emoji/1f44d.png"
+          },
+          {
+              type: "GreenMountain",
+              content: "Neck, Back, or Joint pain",
+              imageSrc: "https://meanbusiness.com/files/2017/04/Muscles-Back-Shoulders-Illustration.jpg"
+          },
+      ]
+  },
+  {
+    // Group 1
+      question: "What will you be doing after medicating?",
+      answers: [
+          {
+              type: "GreenMountain",
+              content: "Taking a walk or bike ride",
+              imageSrc: "https://meanbusiness.com/shepherd/src/svg/gifs/Bey-RidingBike.gif"
+          },
+          {
+              type: "Palmetto",
+              content: "Getting through some chores",
+              imageSrc: "https://meanbusiness.com/files/2017/05/Laundry-Handstand.gif"
+          },
+          {
+              type: "Bluebird",
+              content: "Relaxing",
+              imageSrc: "https://meanbusiness.com/shepherd/src/svg/RelaxedReading-Woman-BareFeet.jpg"
+          },
+      ]
+  },
+  {
+	// Group 1
+      question: "How do you prefer taking your meds?",
+      answers: [
+          {
+              type: "Bluebird",
+              content: "Puffing it",
+              imageSrc: "https://meanbusiness.com/files/2017/05/Woman-Mouth-Smoke-300.gif"
+          },
+          {
+              type: "GreenMountain",
+              content: "Pilling it",
+              imageSrc: "https://meanbusiness.com/files/2017/05/pillbox-weekly.jpg"
+              
+          },
+          {
+              type: "Palmetto",
+              content: "Putting it in Food",
+              imageSrc: "https://meanbusiness.com/files/2017/05/Blending-Fruit.gif"
+          },
+      ]
+  },
+
+];
+
+export default quizQuestions;
