58254d51bf8f2507b3aa5c1ada9376938495a91c
Steve McNally <smcnally@gmail.com>
2017-05-25 16:21:06 -0400

    sponsor links, images

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -22,6 +22,35 @@ function AnswerOption(props) {
     </li>
   );
 
+/**
+
+	170521 - Have to work on the checkboxes,
+	and on App.js:71 (handleAnswerSelected) 
+	to break out the calc scores part from the move to the next q part
+
+
+    return (
+    <li className="answerOption">
+      <input
+        type="checkbox"
+        className="hidden radio-label"
+        className="button-label"
+        name="checkboxGroup"
+        checked={props.answerType === props.answer}
+        id={props.answerType}
+        value={props.answerType}
+        disabled={props.answer}
+        onChange={props.onAnswerSelected}        
+      />
+      <label className="button-label" htmlFor={props.answerType}>
+        {props.answerContent}
+        <p /><img src={props.answerImageSrc} alt={props.answer} height="60%" width="60%" />
+      </label>
+    </li>
+  );
+  **/
+
+  
 }
 
 AnswerOption.propTypes = {
