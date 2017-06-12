c88991b802dedce4de8bd7f2200ef2f15837fdce
Steve McNally <smcnally@gmail.com>
2017-06-12 18:00:04 -0400

    execution-specific files we can swap in and out

---
+++ src/api/CannabIQ-quizQuestions.js
@@ -0,0 +1,63 @@
+var quizQuestions = [
+ 	{
+    // Group 1
+   	  intro: "The Cannabis Industry is Hiring. Match Your Skills & Fit." , 
+  	  // notImplemented scoreLabel: "Cannabis Industry IQ" , 
+      question: "2016's legal cannabis market size in North America was $6.9BN. California's portion of that was:",
+      answers: [
+          {
+              type: "Improving",
+              content: "15%",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/Money-FlippingThrough50s.gif?resize=300,200"
+          },
+          {
+              type: "Improving",
+              content: "25%",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/Money-CashWad-HairBand.jpg?resize=300,200"
+          },
+          {
+              type: "High",
+              content: "31%",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/MakingMoneyAngel-sm.gif?resize=300,200"
+          },
+      ]
+  },
+  {
+      question: "Know why it's growing? As of January 2018:",
+      answers: [
+          {
+              type: "High",
+              content: "Retail sale of Recreational-use cannabis becomes legal  along with current Medical-use sales",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/p124.jpg?resize=300,200"
+          },
+          {
+              type: "Improving",
+              content: "Cannabis can be consumed in any public place",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/Woman-Mouth-Smoke-300.gif?resize=300,200"
+          },
+      ]
+  },
+  {
+    // Group 1
+      question: "By 2021, North America's legal cannabis market is projected to be $21.6BN. CA's estimated portion of that will be:",
+      answers: [
+          {
+              type: "Improving",
+              content: "$8.2 Billion",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/Money-OneBillionDollarNote.jpg?resize=300,200"
+          },
+          {
+              type: "Improving",
+              content: "$15.5 Billion",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/Money-PileOfBills-1sAnd5s.jpg?resize=300,200"
+          },
+          {
+              type: "High",
+              content: "$21.6 Billion",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/files/2017/05/MattressOfMoney-BreakingBad-sm.gif?resize=300,200"
+          },
+      ]
+  }
+ ];
+
+export default quizQuestions;
