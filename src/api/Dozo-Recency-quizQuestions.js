070dc8e37c152552727bce5ee4e37d8410bf5f14
Steve McNally <smcnally@gmail.com>
2018-02-28 21:41:23 -0500

    Dozo execution configs

---
+++ src/api/Dozo-Recency-quizQuestions.js
@@ -0,0 +1,108 @@
+var quizQuestions = [
+  {
+   // Game-wide values
+  intro: "Products Used Within the Last month" , 
+  scoreLabel: "Preference" , 
+  relatedURL0: "https://games.gamepath.io/gamepath/qp/DOZO-chatback.html" , 
+  relatedURLLabel0: "Claim your Gift!" ,
+  relatedURL1: "" , 
+  relatedURLLabel1: "" ,  
+  relatedURL2: "" , 
+  relatedURLLabel2: "" ,
+  relatedURL3: "" , 
+  relatedURLLabel3: "" ,  
+    // Group 1
+      question: "Which type of cannabis product have you used used most recently?",
+      answers: [
+          {
+              type: "Edibles",
+              content: "Edibles",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/05/Insomnia-Woman.jpg?resize=300,200"
+          },
+          {
+              type: "Smoking",
+              content: "Flower / Smoking",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/05/woman-anxiety-300.gif?resize=300,200"
+          },
+		  {
+              type: "Vaping",
+              content: "Vaping",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/06/TinaFey-Self-HighFive.gif?resize=300,200"
+          },
+          {
+              type: "Dabbing",
+              content: "Dabbing",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/04/Muscles-Back-Shoulders-Illustration.jpg?resize=300,200"
+          },
+          {
+              type: "Topicals",
+              content: "Topicals, Lotions, or Ointments",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/04/Muscles-Back-Shoulders-Illustration.jpg?resize=300,200"
+          },
+      ]
+  },
+  {
+    // Group 1
+      question: "Which type of cannabis product have you used most frequently?",
+      answers: [
+          {
+              type: "Edibles",
+              content: "Edibles",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/05/Insomnia-Woman.jpg?resize=300,200"
+          },
+          {
+              type: "Smoking",
+              content: "Flower / Smoking",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/05/woman-anxiety-300.gif?resize=300,200"
+          },
+		  {
+              type: "Vaping",
+              content: "Vaping",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/06/TinaFey-Self-HighFive.gif?resize=300,200"
+          },
+          {
+              type: "Dabbing",
+              content: "Dabbing",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/04/Muscles-Back-Shoulders-Illustration.jpg?resize=300,200"
+          },
+          {
+              type: "Topicals",
+              content: "Topicals, Lotions, or Ointments",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/04/Muscles-Back-Shoulders-Illustration.jpg?resize=300,200"
+          },
+      ]
+  },
+  {
+	// Group 1
+      question: "Which type of cannabis product do you generally prefer best?",
+      answers: [
+          {
+              type: "Edibles",
+              content: "Edibles",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/05/Insomnia-Woman.jpg?resize=300,200"
+          },
+          {
+              type: "Smoking",
+              content: "Flower / Smoking",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/05/woman-anxiety-300.gif?resize=300,200"
+          },
+		  {
+              type: "Vaping",
+              content: "Vaping",
+              imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/06/TinaFey-Self-HighFive.gif?resize=300,200"
+          },
+          {
+              type: "Dabbing",
+              content: "Dabbing",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/04/Muscles-Back-Shoulders-Illustration.jpg?resize=300,200"
+          },
+          {
+              type: "Topicals",
+              content: "Topicals, Lotions, or Ointments",
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/04/Muscles-Back-Shoulders-Illustration.jpg?resize=300,200"
+          },
+      ]
+  },
+];
+
+export default quizQuestions;
