c1cf9c6e5e9fca0eea6b4ec3e4369483289602b6
Steve McNally <smcnally@gmail.com>
2018-05-20 13:38:38 -0400

    Updated with soundSrc

--- src/api/microdose-quizQuestions.js
+++ src/api/microdose-quizQuestions.js
@@ -13,20 +13,24 @@ var quizQuestions = [
   relatedURLLabel2: "Marketing, Retail, & Business" , 
     // Group 1
       question: "What's the recommended amount of THC for a Microdose?",
+      soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/SynthChordsCm11-PercTVOn.m4a" ,
       answers: [
           {
               type: "Magical",
               content: "1 - 5 mg",
+              soundSrc: "https://meanbusiness.com/gamepath/qubodup-megaswosh1.wav" ,
               imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/06/IMG_7533.jpg?crop=225,188"
           },
           {
               type: "Charming",
               content: "10 - 25 mg",
+              soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/qubodup-megaswosh1.wav" ,
               imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/06/IMG_7538.jpg?crop=225,188"
           },
           {
               type: "Charming",
               content: "50-100 mg",
+              soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/qubodup-megaswosh1.wav" ,
               imageSrc: "https://i0.wp.com/extroverting.com/wp-content/uploads/sites/3/2017/07/Bey-Laff-.gif?resize=225,188"
           },
       ]
@@ -34,6 +38,8 @@ var quizQuestions = [
   {
     // Group 1
       question: "What dosage do you get smoking a joint or bong?",
+      soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/qubodup-megaswosh1.wav" ,
+      soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/SynthChordsCm11-PercTVOn.m4a" ,
       answers: [
           {
               type: "Charming",
@@ -55,6 +61,7 @@ var quizQuestions = [
     {
     // Group 1
       question: "Anecdotal and medical studies have suggested microdosing helps with",
+      soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/Hit_Prommer-Simon_Carter.m4a" ,
       answers: [
           {
               type: "Charming",
@@ -64,7 +71,7 @@ var quizQuestions = [
           {
               type: "Charming",
               content: "Anxiety, Stress, Depression",
-              imageSrc: "https://i0.wp.com/meanbusiness.com/shepherd/src/svg/gifs/Dinner-With-Friends.gif?resize=225,188"
+              imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2018/05/Dinner-With-Friends.gif?resize=225,188"
           },
           {
               type: "Magical",
