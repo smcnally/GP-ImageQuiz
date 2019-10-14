7d2dbce36f067cff52874c729eed139ffc4b23ad
Steve McNally <smcnally@gmail.com>
2019-10-14 11:25:59 -0700

    Sept 20 - 30 executions

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,109 +1,104 @@
 var quizQuestions = [
     {
         // Game-wide values
-        intro: "CBD Dosage Calculator",
-        scoreLabel: "<em>Your Best Life Is Inside</em>",
+        intro: "Which Brand is Yours?",
+        scoreLabel: "<em>Your Brands</em>",
         // relatedURL0: "https://games.gamepath.io/gamepath/qp/DOZO-chatback.html",
         // relatedURLLabel0: "Learn more benefits & Take Control of your Experience",
         relatedURL1: "",
         relatedURLLabel1: "",
         relatedURL2: "",
         relatedURLLabel2: "",
-        // relatedURL3: "https://lucentbotanicals.com/blogs/news/how-to-find-right-cbd-dose",
-        // relatedURLLabel3: "How to Find the Right CBD Dose for You",
+        relatedURL3: "",
+        relatedURLLabel3: "",
         // Group 1
-        question: "How much CBD should you take? Answer these simple questions and get our best recommendation.",
+        question: "Do you ever treat yourself to a joint/vape/dab or five?",
         soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/AMBPC_026.m4a",
         multiAnswerSoundSrc: "",
         // imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2018/01/Roller_Broad_City.gif??resize=220,125",
-        imageSrc: "https://gamepath.io/wp-content/uploads/sites/4/2018/11/Lucent-Kirby-I-800k.gif",
+        imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2018/01/Roller_Broad_City.gif",
         answers: [
             {
-                type: "Average",
-                content: " GET STARTED >>",    
+                type: { "Medicine-seeker": 1,  "Parent": 1 },
+                content: " I would like to more often",    
             },
+            {
+                type: { "Millenial": 1,  "cultured": 1 },
+                content: "I do!",
+                imageSrc: ""
+              },
         ]
     },
     {
         // Group 1
-          question: "No surprise, smaller people tend to need less CBD to get the same effect. So what’s your weight range?",
+        question: "Are you Experienced?",
+        imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2017/07/Woman-Mouth-Smoke-300.gif?resize=200,200",
+        soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/AMBPC_026.m4a",
+        //soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/Imptomb06-Tabla_Dolak.m4a",
+        answers: [
+            {
+                type: { "Millennial": 1,  "Cultured": 1 },
+                content: "I consume regularly",
+                imageSrc: ""
+            },
+            {
+                type: { "Medicine-seeker": 1,  "Parent": 1 },
+                content:  "I've consumed in the past, but not in awhile",
+    
+            },
+            {
+                type: { "Parent": 1, "Medicine-seeker": 1 },
+                content: "I'm looking to try",
+                imageSrc: ""
+            },
+    ]
+    },
+    {
+        // Group 1
+          question: "What's your take on cannabis culture?",
           imageSrc: "",
           soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/AMBPC_026.m4a",
           answers: [
               {
-                type: { "Novice": 1,  "Sensitive": 0 },
-                content: "Under 110 lbs (50 kg)",
+                type: { "Millennial": 1,  "Sensitive": 0 },
+                content: "Cannabis is part of the culture, sure.",
                 imageSrc: ""
               },
               {
-                type: { "Novice": 1,  "Sensitive": 0 },
-                content: "110 - 150 lbs (50 - 70 kg)",
+                type: { "Medicine-seeker": 1,  "Parent": 1 },
+                content: "It's like Medicine is part of the culture.",
                 imageSrc: ""
               },
               {
-                type: { "Average": 1, "Heavy": 1 },
-                content: "150 - 220 lbs (70 - 100 kg)",
+                type: { "Millennial": 1, "cultured": 1 },
+                content: "Like the Cocktail Culture?",
                 imageSrc: ""
               },
-              {
-                type: { "Average": 0, "Heavy": 1 },
-                content: "More than 220 lbs (100 kg)",
-                imageSrc: ""
-            },
           ]
       },
     {
         // Group 1
-        question: "How strong an effect are you looking for? For example, if you’re looking for pain relief, how severe is the pain? Or, if you are looking for a sense of calm, how anxious are you?",
+        question: "How strong an effect are you looking to achieve?",
         imageSrc: "",
-        soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/AMBPC_026.m4a",
+        soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/SynthChordsCm11-PercTVOn.m4a",
         answers: [
             {
-                type: { "Novice": 1,  "Sensitive": 0 },
+                type: { "Parent": 1,  "Medicine-seeker": 1 },
                 content: "A small boost",
                 imageSrc: ""
             },
             {
-                type: { "Sensitive": 0, "Average": 1 },
+                type: { "Medicine-seeker": 1, "Millennial": 1 },
                 content: "A moderate but significant effect",
 
             },
             {
-                type: { "Average": 0, "Heavy": 1 },
+                type: { "Millennial": 0, "cultured": 1 },
                 content: "A strong effect",
                 imageSrc: ""
             },
 ]
 },
-{
-    // Group 1
-    question: "Have you taken CBD before? If so, how sensitive to the effects did you feel?",
-    imageSrc: "https://i0.wp.com/meanbusiness.com/wp-content/uploads/2018/08/Dog_Going_Upstairs_Backward.gif?resize=220,125",
-    soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/SynthChordsCm11-PercTVOn.m4a",
-    //soundSrc: "https://meanbusiness.com/gamepath/AmbientHits/fromCMMag/Imptomb06-Tabla_Dolak.m4a",
-    answers: [
-        {
-            type: { "Novice": 1,  "Sensitive": 0 },
-            content: "Never taken CBD / More sensitive",
-            imageSrc: ""
-        },
-        {
-            type: { "Novice": 1, "Average": 1 },
-            content: "Fairly sensitive",
-
-        },
-        {
-            type: { "Average": 1, "Heavy": 0 },
-            content: "Average",
-            imageSrc: ""
-        },
-        {
-            type: { "Average": 0, "Heavy": 1 },
-            content: "Not sensitive",
-            imageSrc: ""
-        },
-]
-}
 ];
 
 export default quizQuestions;
